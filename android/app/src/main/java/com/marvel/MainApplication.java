fe5a74f71c32cb820711ab87df76b545353d0b48
M0hamedSaad <s3adoon.ms@gmail.com>
2021-09-18 18:53:19 +0200

    Details Screen with Animated Sticky Header.

--- android/app/src/main/java/com/marvel/MainApplication.java
+++ android/app/src/main/java/com/marvel/MainApplication.java
@@ -4,12 +4,18 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+  import com.facebook.react.bridge.JSIModulePackage; // <- add
+  import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,10 +35,16 @@ public class MainApplication extends Application implements ReactApplication {
           return packages;
         }
 
+    
         @Override
         protected String getJSMainModuleName() {
           return "index";
         }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+        return new ReanimatedJSIModulePackage(); // <- add
+      }
       };
 
   @Override
--- src/services/useGet.js
+++ src/services/useGet.js
@@ -5,32 +5,30 @@ const apikey = '24432f2a00a2463c494eaf69f9b4d45e';
 const ts = 'ts';
 const baseUrl = 'https://gateway.marvel.com/v1/public';
 
-let result = []
+let result = [];
 
-export const useGet = async (path, id, params, isPagination) => {
+export const useGet = async (path, id, params) => {
     let url = '';
     if (id) url = baseUrl + path + '/' + id
     else url = baseUrl + path
 
     if (params == undefined) params = {}
-    if (isPagination == undefined) result = []
-   
+    if (params?.offset == undefined || params?.offset == 0) result = []
+
     params.hash = hash;
     params.apikey = apikey;
     params.ts = ts;
-
-
-
-    console.log({ url, params });
+    // console.log({ url, params });
     try {
         let res = await axios.get(url, { params });
-        console.log({ res });
-        result = [...result, res.data];
+        console.log(res);
+        result = [...result, ...res.data.data.results];
         return result;
 
     } catch (error) {
         console.log('ERROR', error?.response ?
             error?.response : error);
-        return false;
+        return null;
     }
+
 }
